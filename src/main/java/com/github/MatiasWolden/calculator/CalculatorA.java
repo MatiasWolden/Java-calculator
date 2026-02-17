a1fc1c77179f5066d8aaafc9e0e7020b441594f4
H184600 <184600@stud.hvl.no>
2026-02-17 20:15:38 +0100

    Start

---
+++ src/main/java/com/github/MatiasWolden/calculator/CalculatorA.java
@@ -0,0 +1,17 @@
+package com.github.MatiasWolden.calculator;
+
+import javax.swing.*;
+
+public class CalculatorA {
+
+    JFrame frame = new JFrame("Calculator");
+
+
+    CalculatorA() {
+    }
+
+    public static void main(String[] args) {
+
+        CalculatorA calculator = new CalculatorA();
+    }
+}
